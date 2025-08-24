0b266b1d04cccb163aaf281232482800e10ffddc
Kavya-hjb217 <barodiakavya3000@gmail.com>
2025-08-24 21:21:24 +0530

    Backend DB setup with convex authentication

---
+++ components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SignedIn ,SignedOut,SignInButton,SignUpButton,UserButton} from '@clerk/nextjs';
+
+const Header = () => {
+  return (
+    <div>
+      
+         <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          
+        </div>
+      )
+    }
+
+export default Header;
