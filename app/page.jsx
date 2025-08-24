1efbd7da997baddad3f00995c0824b03e85c9a74
Kavya-hjb217 <barodiakavya3000@gmail.com>
2025-08-25 03:14:42 +0530

    Fixed layout in header & improved responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div >
-      <h1>Welcome to XpnsOne</h1>
-      <p>Your one-stop solution for expense tracking.</p>
+    <div  className="pt-16">
+      Welcome to XpnsOne
+      <br />
       <Button variant="destructive">Get Started</Button>
     </div>
   );
--- convex/schema.js
+++ convex/schema.js
@@ -2,12 +2,16 @@ import { defineSchema, defineTable } from 'convex/server';
 import{ v }from 'convex/values';
 
 
+
 export default defineSchema({
-  // Define your schema here
-  users: defineTable({
-    name:v.string(),
-    email:v.string(),
-    tokenIdentifier:v.string(),
-    imageUrl:v.optional(v.string()),
-  }),
+  // Define your schema here
+  users: defineTable({
+    name:v.string(),
+    email:v.string(),
+    tokenIdentifier:v.string(),
+    imageUrl:v.optional(v.string()),
+  }).index("by_token",["tokenIdentifier"])
+    .index("by_email",["email"]),
+//     .searchIndex("search_name",{searchFields:["name"]})
+//     .searchIndex("search_email",{searchFields:"email"}),
 });
