a3e5c7a657f64288393da0362560032e71614de2
Kavya-hjb217 <barodiakavya3000@gmail.com>
2025-08-24 22:48:31 +0530

    configured convex and clerk together

---
+++ convex/schema.js
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from 'convex/server';
+import{ v }from 'convex/values';
+
+
+export default defineSchema({
+  // Define your schema here
+  users: defineTable({
+    name:v.string(),
+    email:v.string(),
+    tokenIdentifier:v.string(),
+    imageUrl:v.optional(v.string()),
+  }),
+});
