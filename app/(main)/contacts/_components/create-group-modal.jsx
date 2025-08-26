55226b472c8bae3e6c7713391467ed6af3e694d2
Kavya-hjb217 <barodiakavya3000@gmail.com>
2025-08-27 03:46:42 +0530

    saved groups and contacts page for future use.

---
+++ app/(main)/contacts/_components/create-group-modal.jsx
@@ -0,0 +1,25 @@
+import { DialogFooter } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle,DialogDescription } from "@/components/ui/dialog";
+import React from "react";
+
+const CreateGroupModal = ({isOpen,onClose,onSuccess}) => {
+  
+    return (
+    <Dialog open={isOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      
+      <DialogFooter>Footer</DialogFooter>
+      
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateGroupModal;
